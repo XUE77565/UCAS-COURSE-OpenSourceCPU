44db7b65dac7fbeefb548a136b0a57bacfda166a
XueYizhou <xueyizhou23@mails.ucas.ac.cn>
2026-05-18 18:58:04 +0800

    implemented more insts in pa2 pass all tests

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,7 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 #ifdef CONFIG_WATCHPOINT
-  printf("Checking watchpoints...\n");
+  //printf("Checking watchpoints...\n");
   bool changed = false;
   changed = check_watchpoints();
   if(changed) {
