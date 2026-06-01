8936658542983fbcd6f5134a7c1f32165cc5e89c
XueYizhou <xueyizhou23@mails.ucas.ac.cn>
2026-06-01 14:49:25 +0800

    add more insts

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,23 @@
 #include <am.h>
 #include <nemu.h>
 
-void __am_timer_init() {
+static uint64_t boot_time = 0;
+
+static uint64_t read_time() {
+  uint32_t high = inl(RTC_ADDR + 4);
+  uint32_t low = inl(RTC_ADDR);
+  return ((uint64_t)high << 32) | low;
 }
 
+
+void __am_timer_init() {
+  boot_time = read_time();
+}
+
+//由于在nemu中的timer是在offset=4的时候才获取真实事件，所以应该先访问高32
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  //从RTC_ADDR处获取长度为8字节的MMIO空间，来获取64位当前的时间
+  uptime->us = read_time() - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
