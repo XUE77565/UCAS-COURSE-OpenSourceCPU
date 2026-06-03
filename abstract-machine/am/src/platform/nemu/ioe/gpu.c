fe2f524c5e3d7b0c9e0cb306672f8e72b265fe3b
XueYizhou <xueyizhou23@mails.ucas.ac.cn>
2026-06-03 14:44:43 +0800

    implemented gpu

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,20 +1,44 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t screen_wh = inl(VGACTL_ADDR);
+  uint32_t h = screen_wh & 0xffff;
+  uint32_t w = screen_wh >> 16;
+  printf("GPU screen: %d * %d\n", w, h);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for(i = 0; i < w * h; i++) {
+    fb[i] = i;
+  }
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t screen_wh = inl(VGACTL_ADDR);
+  uint32_t h = screen_wh & 0xffff;
+  uint32_t w = screen_wh >> 16;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = w * h * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t screen_wh = inl(VGACTL_ADDR);
+  uint32_t *fbctl = (uint32_t *)FB_ADDR;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  for (int j = 0; j < h; j++) {
+    for (int i = 0; i < w; i++) {
+      fbctl[(y + j) * (screen_wh >> 16) + (x + i)] = pixels[j * w + i];
+    }
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
